e3b968dfd911ba0d8bf00d91fb90080be28fd10e
diablohumastudioadmin <admin@diablohumastudio.com>
2026-03-02 18:34:27 -0500

    new plugin for only bulk editing and visual crud data created base files

---
+++ addons/diablohumastudio/visual_resources_editor/visual_resources_editor_toolbar.gd
@@ -0,0 +1,23 @@
+@tool
+class_name VisualResourcesEditorToolbar
+extends DiablohumaStudioToolMenu
+
+const VisualResourcesEditorWindowPksc = preload("uid://b6ssn0jpljw4r")
+var visual_resources_editor_window: Window
+
+func _enter_tree() -> void:
+	add_item("Launch Visual Editor", 0, KEY_F3)
+	id_pressed.connect(_on_menu_id_pressed)
+
+func _exit_tree() -> void:
+	pass
+
+func _on_menu_id_pressed(id: int):
+	match id:
+		0:
+			open_visual_editor_window()
+
+func open_visual_editor_window():
+	visual_resources_editor_window = VisualResourcesEditorWindowPksc.instantiate()
+	EditorInterface.get_base_control().add_child(visual_resources_editor_window)
+	visual_resources_editor_window.popup_centered()
