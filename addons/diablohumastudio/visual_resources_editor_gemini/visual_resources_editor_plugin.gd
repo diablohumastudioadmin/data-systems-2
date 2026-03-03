8954d0ef2cdd1f553ec7931bd8157e6124f31166
diablohumastudioadmin <admin@diablohumastudio.com>
2026-03-02 19:44:33 -0500

    plugin for visual CRUD and visual editing boiler plate created for gemini

---
+++ addons/diablohumastudio/visual_resources_editor_gemini/visual_resources_editor_plugin.gd
@@ -0,0 +1,14 @@
+@tool
+extends DiabloHumaStudioPlugin
+
+const TOOLBAR_MENU_NAME: String = "VisualResourcesEditor"
+
+func _enter_tree() -> void:
+	add_toolbar_menu()
+
+func add_toolbar_menu():
+	var tool_bar_menu := VisualResourcesEditorToolbar.new()
+	MainToolbarPlugin.add_toolbar_shubmenu(TOOLBAR_MENU_NAME, tool_bar_menu, self)
+
+func _exit_tree() -> void:
+	MainToolbarPlugin.remove_toolbar_submenu(TOOLBAR_MENU_NAME, self)
