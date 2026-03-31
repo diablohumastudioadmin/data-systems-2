08c0cc47892811930bdd1a36d5daf15a17300253
diablohumastudioadmin <admin@diablohumastudio.com>
2026-03-30 19:44:26 -0500

    editor filesystem listener added (not used yet) for managing sincronizing between EditorFileSystem script_classes_updated and filesystem_changed signals

---
+++ addons/diablohumastudio/visual_resources_editor/core/editor_filesystem_listener.gd
@@ -0,0 +1,37 @@
+@tool
+class_name EditorFileSystemListener
+extends RefCounted
+
+signal script_classes_updated()
+signal filesystem_changed()
+
+var _prevent_fs_changed: bool = false
+
+func start() -> void:
+	var efs: EditorFileSystem = EditorInterface.get_resource_filesystem()
+	if efs:
+		if not efs.script_classes_updated.is_connected(_on_script_classes_updated):
+			efs.script_classes_updated.connect(_on_script_classes_updated)
+		if not efs.filesystem_changed.is_connected(_on_filesystem_changed):
+			efs.filesystem_changed.connect(_on_filesystem_changed)
+
+
+func stop() -> void:
+	var efs: EditorFileSystem = EditorInterface.get_resource_filesystem()
+	if efs:
+		if efs.script_classes_updated.is_connected(_on_script_classes_updated):
+			efs.script_classes_updated.disconnect(_on_script_classes_updated)
+		if efs.filesystem_changed.is_connected(_on_filesystem_changed):
+			efs.filesystem_changed.disconnect(_on_filesystem_changed)
+
+
+func _on_script_classes_updated() -> void:
+	_prevent_fs_changed = true
+	script_classes_updated.emit()
+
+
+func _on_filesystem_changed() -> void:
+	if _prevent_fs_changed:
+		_prevent_fs_changed = false
+		return
+	filesystem_changed.emit()
